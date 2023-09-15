5670ef2217b687780b5630e55a38cc8b1ef78e85
Manolya Tamimi <tamimimanolya@gmail.com>
2023-09-15 19:17:19 +0300

    making rough design

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import AddPage from './components/AddPage/addpage.component';
 
 function App() {
   return (
     <div className="App">
-      
+      <AddPage/>
     </div>
   );
 }
---
+++ src/components/AddPage/addpage.component.jsx
@@ -0,0 +1,9 @@
+import SearchMembersForm from "../SearchMembersForm/SearchMembersForm.component";
+const AddPage = () => {
+    return <>
+        <h4>Add memebers to Front-end development team</h4>
+        <SearchMembersForm />
+    </>
+}
+
+export default AddPage;
